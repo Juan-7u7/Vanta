d96a1930037a0f900dea1c3f69ed2da6155c59d7
Juan <ezio4213@gmail.com>
2026-03-14 13:29:36 -0600

    Initial Vite and Tailwind v4 setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#281C59_100%)] dark:[background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#281C59_100%)] dark:items-center dark:px-5 dark:py-24"></div>
+      
+      <div className="min-h-screen relative z-0">
+        {/* Tu contenido irá aquí */}
+      </div>
+    </>
+  )
+}
+
+export default App
